bb392eaf2e4c97a2837b54dc47474a93fff627f2
Megan Zhang <mzhang88@live.unc.edu>
2019-01-17 14:57:49 -0500

    Update A1Adept.java

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,6 +8,16 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 		
 		// Your code goes here.
+		scan.next();
+		char firstLetter = scan.next().charAt(0);
+		scan.next();
+		scan.next();
+		int bananas = scan.nextInt()
+		scan.next();
+		double bananasCost = scan.nextDouble();
+		
+		System.out.println(firstLetter);
+		System.out.println(bananas * bananasCost);
 	}
 	
 	// You can define / use static helper methods here.
