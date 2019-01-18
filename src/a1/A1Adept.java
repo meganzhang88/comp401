c6061f916c87b74c2f74a9266bee18d230d9190d
Megan Zhang <mzhang88@live.unc.edu>
2019-01-18 17:44:53 -0500

    Update A1Adept.java
    complete

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,6 +1,7 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class A1Adept {
 
@@ -8,6 +9,71 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 		
 		// Your code goes here.
+		String biggest = "biggest";
+		String smallest = "smallest";
+		int itemsInStore = scan.nextInt();
+		
+		String[] itemsArray = new String[itemsInStore];
+		double[] pricesArray = new double[itemsInStore];
+		
+		for (int i = 0; i < itemsInStore; i++) {
+			String nameOfItem = scan.next();
+			double price = scan.nextDouble();
+			
+			itemsArray[i] = nameOfItem;
+			pricesArray[i]= price; 
+		}
+		
+		int customers = scan.nextInt();
+		double[] totals = new double[customers];
+		
+		double bigAmount = 0;
+		double smallAmount = 0;
+		
+		for (int i = 0; i < customers; i++) {
+		double newTotal = 0;
+		double oldTotal = 0;
+			String first = scan.next();
+			String last = scan.next();
+			int itemsBought = scan.nextInt();
+			
+			for (int j = 0; j < itemsBought; j++) {
+				int quantity = scan.nextInt();
+				String itemName = scan.next();
+
+				for (int k = 0; k < itemsArray.length; k++) {
+					if (itemName.equals(itemsArray[k])) {
+						newTotal = newTotal + pricesArray[k] * quantity;
+					}
+				}
+			}
+
+			
+			if (newTotal > bigAmount) {
+				biggest = first + " " + last;
+				bigAmount = newTotal;
+				totals[i] = newTotal; 
+			} else if (smallAmount == 0) {
+				smallAmount = newTotal;
+				smallest = first + " " + last;
+				totals[i]= newTotal; 
+			} else if (newTotal < smallAmount) {
+				smallAmount = newTotal;
+				smallest = first + " " + last;
+				totals[i] = newTotal;
+			}
+		}
+		
+		double sum = 0;
+		for (int l = 0; l < totals.length; l++) {
+			sum = sum + totals[l];
+		}
+		
+		double average = sum / totals.length;
+		
+		System.out.println("Biggest: " + biggest + "(" + bigAmount + ")");
+		System.out.println("Smallest: " + smallest + "(" + smallAmount + ")");
+		System.out.println("Average: " + average);
 	}
 	
 	// You can define / use static helper methods here.
